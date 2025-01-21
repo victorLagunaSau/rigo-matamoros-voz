1fb9b5ba1bd8248783f3783219b3bcb0a8086846
victorLagunaSau <viktor.laguna@gmail.com>
2025-01-20 20:54:32 -0600

    Inicial vercel

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const { VoiceResponse } = require('twilio').twiml;
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
+// Ruta para manejar POST en /voice-webhook
 app.post('/voice-webhook', (req, res) => {
     const twiml = new VoiceResponse();
     twiml.say('Hola, esta es una prueba de Twilio con Vercel y Node.js.');
@@ -13,6 +13,5 @@ app.post('/voice-webhook', (req, res) => {
     res.send(twiml.toString());
 });
 
-app.listen(port, () => {
-    console.log(`Servidor escuchando en el puerto ${port}`);
-});
+// Exportar la app para que Vercel la use
+module.exports = app;
